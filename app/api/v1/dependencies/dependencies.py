3bda1db51220238c1086fa70af392fee4140815d
vuongnguyendev <vuongvietashd@gmail.com>
2026-05-22 11:36:58 +0700

    fix(be): fix internal 500 error

--- app/api/v1/dependencies/dependencies.py
+++ app/api/v1/dependencies/dependencies.py
@@ -4,12 +4,11 @@ from fastapi import Depends, HTTPException, status, Header
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from app.core.database import SessionLocal
-from app.models.user import User, Role
-from app.models.permission import Permission
+from app.models.user import User, Role, Permission
 from app.core.config import settings
 from jose import jwt, JWTError
 from app.models.device import DeviceManagementLog
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f"{settings.API_V1_STR}/auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f"{settings.API_V1_STR}/auth/swagger-login")
 
 def get_db():
     db = SessionLocal()
@@ -46,14 +45,14 @@ def get_user_roles(current_user: User = Depends(get_current_user), db: Session =
     ).all()
     return [r[0] for r in roles]
 
-def require_admin_role(current_user: User = Depends(get_current_user)):
-    return require_role("admin")
+def require_admin_role(current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+    return require_role("admin", current_user, db)
 
-def require_moderator_role(current_user: User = Depends(get_current_user)):
-    return require_role("moderator")
+def require_moderator_role(current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+    return require_role("moderator", current_user, db)
     
 def require_role(role: str, current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
-    roles = get_user_roles()
+    roles = get_user_roles(current_user, db)
     if role not in roles:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
@@ -78,6 +77,16 @@ def validate_user_device_ownership(device_id: uuid.UUID, current_user: User = De
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="The user doesn't have enough privileges")
     return device_id
     
+
+def validate_user_ownership(user_id: uuid.UUID, device_id: uuid.UUID, db: Session):
+    device_ids = db.query(DeviceManagementLog.device_id).filter(
+        DeviceManagementLog.user_id == user_id
+    ).filter(
+        DeviceManagementLog.removed_at == None
+    ).all()
+    if not device_ids or device_id not in [d[0] for d in device_ids]:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="The user doesn't have enough privileges")
+
 
         
     
